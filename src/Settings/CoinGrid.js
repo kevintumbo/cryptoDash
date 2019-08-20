d8c0c653cb197b9fda65fce4d9d521cad89e6ae2
kevintumbo <kevin.tumbo39@gmail.com>
2019-08-20 15:29:31 +0300

    add responsiveness

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,9 +5,10 @@ import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
   grid-gap: 15px;
   margin-top: 40px;
+
 `
 
 function getCoinsToDisplay(coinList, topSection, favorites){
